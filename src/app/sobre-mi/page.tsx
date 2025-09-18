e1df138055fc8b9a297fc3e06129375bb471aede
sebas2dev <sebas2dev@gmail.com>
2025-09-18 11:23:38 -0500

    update

--- src/app/sobre-mi/page.tsx
+++ src/app/sobre-mi/page.tsx
@@ -18,32 +18,6 @@ export const metadata = {
 export default function SobreMi() {
   return (
     <PageTransition>
-      {/* <Head>
-        <title>
-          Conoce a Cata Suárez | Educación y Parentalidad Consciente
-        </title>
-        <meta
-          name="description"
-          content="Con más de 10 años de experiencia, Cata Suárez comparte su misión de transformar la crianza y la educación con programas, talleres y conferencias."
-        />
-        <meta
-          property="og:title"
-          content="Conoce a Cata Suárez | Educación y Parentalidad Consciente"
-        />
-        <meta
-          property="og:description"
-          content="Con más de 10 años de experiencia, Cata Suárez comparte su misión de transformar la crianza y la educación con programas, talleres y conferencias."
-        />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content="https://catasuarezeducacion.com/sobre-mi"
-        />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/dqgqrvnnw/image/upload/v1754921395/Banner_c6lnhl.jpg"
-        />
-      </Head> */}
       <main>
         <SobreMiHero />
         <SobreMiInfo />
